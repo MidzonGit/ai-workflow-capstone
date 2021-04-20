fb46d558bc902b4e581c4fdb81efec4faf4cf66b
MidzonGit <pg14mithun_c@mandevian.com>
2021-04-21 00:51:38 +0530

    Add files via upload

--- logger.py
+++ logger.py
@@ -24,21 +24,18 @@ def setup_logger(name, log_file, level=logging.INFO):
     return logger
 
 
-def update_train_log(tag,period,rmse,runtime,MODEL_VERSION,MODEL_VERSION_NOTE,test=False):  
-    log_msg=''
+def update_train_log(msg):  
+    log_msg=msg
     train_logger = setup_logger('train', 'logs/train.log')
     train_logger.info(str(log_msg))
     
 
-def update_predict_log(country, y_pred,y_proba,target_date,runtime,MODEL_VERSION,test=False):    
-    log_msg=''
+def update_predict_log(msg):    
+    log_msg=msg
     predict_logger = setup_logger('predict', 'logs/predict.log')
     predict_logger.info(str(log_msg))
     
 
 if __name__ == "__main__":
-    from model import MODEL_VERSION, MODEL_VERSION_NOTE
-    update_train_log(str((100,10)),"{'rmse':0.5}","00:00:01",
-                     MODEL_VERSION, MODEL_VERSION_NOTE,test=True)
-    update_predict_log("[0]","[0.6,0.4]","['united_states', 24, 'aavail_basic', 8]",
-                       "00:00:01",MODEL_VERSION, test=True)
+    update_train_log('Train logger')
+    update_predict_log("Predict logger")
